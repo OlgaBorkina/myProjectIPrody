ccfb1686f48a4788734a0cfaaa82e3096ab4f794
OlgaBorkina <ovBorkina@gmail.com>
2025-06-26 21:39:39 +0300

    feat : creat method shakeSort to main

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,39 @@ public class Main {
         System.out.println("Sourse  " + Arrays.toString(sourse));
         System.out.println("Target  " + Arrays.toString(target));
         arrayCopy(sourse,target);
+
+        int [] array = new int []{78,5,9,14,7,3,11,0,7,4};
+        shakerSort(array);
+    }
+
+    public static  void shakerSort(int[] arr){
+        System.out.println( "Start  "+ Arrays.toString(arr));
+       boolean flagLeft = true;
+       boolean flagRight = true;
+
+       while (flagLeft || flagRight){
+           flagLeft = false;
+           flagRight = false;
+          for (int i = 1 ; i < arr.length; i++){
+
+            if(arr[i]<arr[i-1]){
+               int temp = arr[i];
+               arr[i] = arr[i-1];
+               arr[i-1] = temp;
+               flagLeft = true;
+            }
+        }
+            for (int j = arr.length-1; 0 < j ; j--) {
+
+                if(arr[j]<arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    flagLeft = true;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
 
 
